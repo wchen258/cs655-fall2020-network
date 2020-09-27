6a6f8188dc103c29e921c58e49b1982ed7c6bf86
kokoro <39005722+wchen258@users.noreply.github.com>
2020-09-27 01:34:37 -0400

    ready: single plot

--- pa1part2/plot.py
+++ pa1part2/plot.py
@@ -6,9 +6,10 @@ Created on Sat Sep 26 22:17:19 2020
 @author: kokoro
 """
 
-import seaborn
+import seaborn as sns
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def get_datas(expname):
     path = os.path.join('results', expname)
@@ -22,11 +23,38 @@ def get_npy(exp_type:str, delay:int, exp_name):
     npy = np.genfromtxt(path, delimiter=',')
     x,y,y_std = npy[:,0], npy[:,1], npy[:,2]
     return x, y, y_std
+
+def genplot(exp_type:str, delay:int, exp_name:str, probes=15):
+    x_label = 'msg_size (byte)' if exp_type=='rtt' else 'msg_size (KB)'
+    y_label = 'avg_rtt (ms)' if exp_type=='rtt' else 'avg_tput (kbps)'
+    x,y,y_err = get_npy(exp_type, delay, exp_name)
+    if exp_type=='tput':
+        x = x/1000
+        y = x / y * (1024/1000)
+        y_err = x / y_err * (1024/1000)
+    
+    plt.plot(x, y, 'or')
+    plt.plot(x, y, '-', color='red')
+    plt.fill_between(x, y - y_err, y + y_err,color='gray', alpha=0.2)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    fname = '_'.join([exp_type, str(delay), exp_name, str(probes)]) + '.png'
+    path = os.path.join('plots', fname)
+    plt.savefig(path)
+    return plt
     
 
 
 
 if __name__=='__main__':
-    exp_name = 'exp1'
     
-    x, y, y_std = get_npy('rtt', 0, exp_name)
+    ## config
+    exp_name = 'exp1'
+    plt.style.use(['seaborn-darkgrid'])
+    
+    ## make plots directory
+    if not os.path.exists('./plots'):
+        os.makedirs('./plots')
+        
+    genplot('tput', 0, 'exp1')
+    
