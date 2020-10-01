0d104a884e74eaf47ea69eb1a7ab8ee779942383
eagleoflqj <liumeo@pku.edu.cn>
2020-10-01 00:24:03 -0400

    clean code

--- document.md
+++ document.md
@@ -36,6 +36,17 @@ hello world!
 * We implement more accurate validation than the provided test servers.
 ## Test
 * To start server on csa1, choose a port (here 58233) and run `python3 server.py 58233`.
-* To start client on csa1 or other devices, run `python3 client.py csa1.bu.edu 58233`
+* To start client on csa1 or other devices, first install `tqdm` by `python3 -m pip install tqdm --user`, then run `python3 client.py csa1.bu.edu 58233`.
 * We also implement a shell script `test.sh` to check whether `server.py` works correctly under different requests. We come up with some testcases (stored in `testcases`). To test any server, run `sh test.sh <Address> <Port>`. Below is the result of our server and the provided test server. As we can see, we passed all testcases, but the provided server failed some cases.
 ![](test.jpg)
+## Environment
+Experiment|Message size(byte)|Delay(ms)|Probes|Client|Server
+-|-|-|-|-|-
+RTT1|1,100,200,400,800,1000|0,1,10,20,50,100|15|Local|csa1
+TPUT1|1K,2K,4K,8K,16K,32K|0,1,10,20,50,100|15|Local|csa1
+RTT2|1,100,200,400,800,1000|0,1,10,20,50,100|15|csa1|Test Server
+TPUT2|1K,2K,4K,8K,16K,32K|0,1,10,20,50,100|15|csa1|Test Server
+* Local: macOS (Darwin 19.4.0)
+* csa1: CentOS (Linux 3.10.0-1127.18.2.el7.x86_64)
+* Test Server: unknown, 128.206.119.42:8888
+## Result
--- pa1part2/plot.py
+++ pa1part2/plot.py
@@ -11,11 +11,6 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-def get_datas(expname):
-    path = os.path.join('results', expname)
-    onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-    # onlyfiles = list(map(lambda x: os.path.join('results', x), onlyfiles))
-    return onlyfiles
 
 def get_npy(exp_type:str, delay:int, exp_name):
     fname = '_'.join([exp_type, str(delay)]) + '.csv'
@@ -24,47 +19,6 @@ def get_npy(exp_type:str, delay:int, exp_name):
     x,y,y_std = npy[:,0], npy[:,1], npy[:,2]
     return x, y, y_std
 
-def genplot(exp_type:str, delay:int, exp_name:str, probes=15):
-    x_label = 'msg_size (byte)' if exp_type=='rtt' else 'msg_size (KB)'
-    y_label = 'avg_rtt (ms)' if exp_type=='rtt' else 'avg_tput (kbps)'
-    x,y,y_err = get_npy(exp_type, delay, exp_name)
-    if exp_type=='tput':
-        x = x/1000
-        y = x / y * (1024/1000)
-        y_err = x / y_err * (1024/1000)
-    
-    plt.plot(x, y, 'or')
-    plt.plot(x, y, '-', color='red')
-    plt.fill_between(x, y - y_err, y + y_err,color='red', alpha=0.095)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    fname = '_'.join([exp_type, str(delay), exp_name, str(probes)]) + '.png'
-    path = os.path.join('plots', fname)
-    plt.savefig(path)
-    return plt
-
-def genplot2(exp_type:str, delay:int, delay2:int, exp_name:str, probes=15):
-    x_label = 'msg_size (byte)' if exp_type=='rtt' else 'msg_size (KB)'
-    y_label = 'avg_rtt (ms)' if exp_type=='rtt' else 'avg_tput (kbps)'
-    x,y,y_err = get_npy(exp_type, delay, exp_name)
-    x2,y2,y_err2 = get_npy(exp_type, delay2, exp_name)
-    if exp_type=='tput':
-        x, x2 = x/1000, x2/1000
-        y, y2 = x / y * (1024/1000), x2 / y2 * (1024/1000)
-        y_err, y_err2 = x / y_err * (1024/1000) , x2/y_err2 * (1024/1000)
-    
-    plt.plot(x, y, 'or')
-    plt.plot(x, y, '-', color='red')
-    plt.fill_between(x, y - y_err, y + y_err,color='red', alpha=0.095)
-    plt.plot(x2, y2, 'ob')
-    plt.plot(x2, y2, '-', color='blue')
-    plt.fill_between(x2, y2 - y_err2, y2 + y_err2,color='blue', alpha=0.095)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    fname = '_'.join([exp_type, str(delay), str(delay2), exp_name, str(probes)]) + '.png'
-    path = os.path.join('plots', fname)
-    plt.savefig(path)
-    return plt
 
 def genplotList(exp_type:str, delays, exp_name:str, probes=15):
     x_label = 'msg_size (byte)' if exp_type=='rtt' else 'msg_size (KB)'
@@ -97,7 +51,6 @@ def genplotList(exp_type:str, delays, exp_name:str, probes=15):
 if __name__=='__main__':
     
     ## config
-    exp_name = 'exp1'
     plt.style.use(['seaborn-darkgrid'])
     SERVER_DELAYS = (0, 1, 10, 20, 50, 100)
     NUMBER_OF_PROBES = 15
