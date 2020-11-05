14b9e0557ba55e1615dae68f8e35834b1234497b
kokoro <39005722+wchen258@users.noreply.github.com>
2020-11-05 18:39:27 -0500

    add rtt retrans exp and plot

--- pa2/plot.py
+++ pa2/plot.py
@@ -29,20 +29,61 @@ def cout_conf_interval(df, Z=1.96):
 if __name__ == '__main__':
     sns.set_style("darkgrid", {"axes.facecolor": ".9"})
 
-    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands'])
+    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
     data = data.iloc[:, :2]
     print("corrupt case interval")
     cout_conf_interval(data)
     plot = sns.pointplot('prob', 'ave_cmt', data=data, dodge=True, join=True)
     fig = plot.get_figure()
-    fig.savefig('output_corrupt.png')
+    fig.savefig('p1plot_cor_cmt.png')
 
     fig.clf()
 
-    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands'])
+    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
     data = data.iloc[:, :2]
     print("loss case interval")
     cout_conf_interval(data)
     plot = sns.pointplot('prob', 'ave_cmt', data=data, dodge=True, join=True)
     fig = plot.get_figure()
-    fig.savefig('output_drop.png')
+    fig.savefig('p1plot_drop_cmt.png')
+
+    fig.clf()
+
+    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = data.iloc[:, [1,3]]
+    print("loss case interval")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'rtt', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('p1plot_drop_rtt.png')
+
+    fig.clf()
+
+    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = data.iloc[:, [1,3]]
+    print("loss case interval")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'rtt', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('p1plot_cor_rtt.png')
+
+
+    fig.clf()
+
+    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = data.iloc[:, [1,4]]
+    print("loss case interval")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'retrans', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('p1plot_drop_retrans.png')
+
+    fig.clf()
+
+    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = data.iloc[:, [1,4]]
+    print("loss case interval")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'retrans', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('p1plot_cor_retrans.png')
