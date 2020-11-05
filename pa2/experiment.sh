841c5f68fc2252b13419ee44faeb2bb7673ed7ea
eagleoflqj <liumeo@pku.edu.cn>
2020-11-05 12:31:16 -0500

    modify arg

--- pa2/experiment.sh
+++ pa2/experiment.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 
 if [ -f drop_exp ]; then
     rm -f drop_exp
@@ -8,14 +7,13 @@ if [ -f corrupt_exp ]; then
     rm -f corrupt_exp
 fi
 
-
 prefix_cmt="Average communication time: "
 echo "Start drop probability experiment"
-for p in $(seq 0.1 0.1 0.9)
+for p in $(seq 0.0 0.1 0.9)
 do
-for i in {1000..1020};do
-    echo 1000 "$p" 0.1 200 8 30 3 "$i" | ./pa2 > temp
-    result=$(cat temp | grep "Average communication time: ")
+for i in {1000..1019}; do
+    echo 100 "$p" 0.1 200 8 15 1 "$i" | ./pa2 > temp
+    result=$(grep "Average communication time: " temp)
     result=${result#$prefix_cmt}
     echo "$result","$p","$i" >> drop_exp
     echo "prob $p rands $i done"
@@ -24,11 +22,11 @@ done
 rm -f temp
 
 echo "Start corrupt probability experiment"
-for p in $(seq 0.1 0.1 0.9)
+for p in $(seq 0.0 0.1 0.9)
 do
-for i in {1000..1020};do
-    echo 1000 0.1 "$p" 200 8 30 3 "$i" | ./pa2 > temp
-    result=$(cat temp | grep "Average communication time: ")
+for i in {1000..1019}; do
+    echo 100 0.1 "$p" 200 8 15 1 "$i" | ./pa2 > temp
+    result=$(grep "Average communication time: " temp)
     result=${result#$prefix_cmt}
     echo "$result","$p","$i" >> corrupt_exp
     echo "prob $p rands $i done"
