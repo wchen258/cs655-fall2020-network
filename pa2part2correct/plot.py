add1506529f3d3cd610e6498089e56d483b00df4
kokoro <39005722+wchen258@users.noreply.github.com>
2020-11-05 20:40:51 -0500

    gbn more stuff

--- pa2part2correct/plot.py
+++ pa2part2correct/plot.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import warnings
 import os
+import warnings
 
 warnings.filterwarnings("ignore")
 
@@ -15,11 +15,11 @@ def conf_interval(arr, Z=1.96):
     return f'{lower:.2f}', f'{upper:.2f}'
 
 
-def cout_conf_interval(df, Z=1.96):
+def cout_conf_interval(df, Z=1.96, col=1):
     d = {}
     for i in range(10):
         start = i*20
-        arr = df.iloc[start:(start+20), 0]
+        arr = df.iloc[start:(start+20), col]
         inter = conf_interval(arr, Z)
         d[f'0.{i}'] = inter
         print(f'0.{i}', inter)
@@ -31,34 +31,34 @@ if __name__ == '__main__':
     if not os.path.exists('../plots'):
         os.makedirs('../plots')
 
+    namelist=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans','tput','gput']
 
     sns.set_style("darkgrid", {"axes.facecolor": ".9"})
 
-    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = pd.read_csv('corrupt_exp', names=namelist)
     data = data.iloc[:, :2]
-    print("corrupt case interval")
-    cout_conf_interval(data)
+    print("GBN corruption vs communication time")
+    cout_conf_interval(data,col=0)
     plot = sns.pointplot('prob', 'ave_cmt', data=data, dodge=True, join=True)
     fig = plot.get_figure()
     fig.savefig('../plots/p2plot_cor_cmt.png')
 
     fig.clf()
 
-    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = pd.read_csv('drop_exp', names=namelist)
     data = data.iloc[:, :2]
-    print("loss case interval")
-    cout_conf_interval(data)
+    print("GBN loss vs communication time")
+    cout_conf_interval(data,col=0)
     plot = sns.pointplot('prob', 'ave_cmt', data=data, dodge=True, join=True)
     fig = plot.get_figure()
     fig.savefig('../plots/p2plot_drop_cmt.png')
 
     fig.clf()
 
-    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = pd.read_csv('drop_exp', names=namelist)
     data.fillna(0, inplace=True)
     data = data.iloc[:, [1,3]]
-    data.loc[data['rtt'] > 100, 'rtt'] = 0
-    print("loss case interval")
+    print("GBN loss vs RTT")
     cout_conf_interval(data)
     plot = sns.pointplot('prob', 'rtt', data=data, dodge=True, join=False)
     fig = plot.get_figure()
@@ -66,11 +66,10 @@ if __name__ == '__main__':
 
     fig.clf()
 
-    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
-    data.fillna(0, inplace=True)
+    data = pd.read_csv('corrupt_exp', names=namelist)
     data = data.iloc[:, [1,3]]
-    data.loc[data['rtt'] > 100, 'rtt'] = 0
-    print("loss case interval")
+    data.fillna(0, inplace=True)
+    print("GBN corrupt vs RTT")
     cout_conf_interval(data)
     plot = sns.pointplot('prob', 'rtt', data=data, dodge=True, join=False)
     fig = plot.get_figure()
@@ -79,9 +78,9 @@ if __name__ == '__main__':
 
     fig.clf()
 
-    data = pd.read_csv('drop_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = pd.read_csv('drop_exp', names=namelist)
     data = data.iloc[:, [1,4]]
-    print("loss case interval")
+    print("GBN loss vs num retransmission")
     cout_conf_interval(data)
     plot = sns.pointplot('prob', 'retrans', data=data, dodge=True, join=True)
     fig = plot.get_figure()
@@ -89,10 +88,56 @@ if __name__ == '__main__':
 
     fig.clf()
 
-    data = pd.read_csv('corrupt_exp', names=['ave_cmt', 'prob', 'rands', 'rtt', 'retrans'])
+    data = pd.read_csv('corrupt_exp', names=namelist)
     data = data.iloc[:, [1,4]]
-    print("loss case interval")
+    print("GBN corrupt vs num retransmission")
     cout_conf_interval(data)
     plot = sns.pointplot('prob', 'retrans', data=data, dodge=True, join=True)
     fig = plot.get_figure()
     fig.savefig('../plots/p2plot_cor_retrans.png')
+
+
+   ## Tput 
+
+    fig.clf()
+
+    data = pd.read_csv('drop_exp', names=namelist)
+    data = data.iloc[:, [1,5]]
+    print("GBN loss vs tput")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'tput', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('../plots/p2plot_drop_tput.png')
+
+    fig.clf()
+
+    data = pd.read_csv('corrupt_exp', names=namelist)
+    data = data.iloc[:, [1,5]]
+    print("GBN corrupt vs tput")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'tput', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('../plots/p2plot_cor_tput.png')
+
+
+    # gput
+
+    fig.clf()
+
+    data = pd.read_csv('drop_exp', names=namelist)
+    data = data.iloc[:, [1,6]]
+    print("GBN loss vs gput")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'gput', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('../plots/p2plot_drop_gput.png')
+
+    fig.clf()
+
+    data = pd.read_csv('corrupt_exp', names=namelist)
+    data = data.iloc[:, [1,6]]
+    print("GBN corrupt vs gput")
+    cout_conf_interval(data)
+    plot = sns.pointplot('prob', 'gput', data=data, dodge=True, join=True)
+    fig = plot.get_figure()
+    fig.savefig('../plots/p2plot_cor_gput.png')
