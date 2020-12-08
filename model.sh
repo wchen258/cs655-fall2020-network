022abdb15e993b00de4be984930c4e7bcf6d371b
eagleoflqj <liumeo@pku.edu.cn>
2020-12-08 12:57:31 -0500

    fix some dist bugs

--- model.sh
+++ model.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python3 -m pip install torchvision
+sudo apt update
+apt install -y python-pip
+python3 -m pip --no-cache-dir install torchvision
 git clone -b final --depth 1 https://github.com/wchen258/cs655-fall2020-network
 cd cs655-fall2020-network
 nohup python3 model.py &
--- server.sh
+++ server.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install -y openjdk-11-jdk
 git clone -b final --depth 1 https://github.com/wchen258/cs655-fall2020-network
 cd cs655-fall2020-network/tomcat/webapps/Servlet/WEB-INF/classes/servletclass
-javac -cp '../../lib/*.jar' Servlet.java
+javac -cp '../../lib/*' Servlet.java
 cd ../../../../../bin
+mkdir -p ../logs
 sudo ./catalina.sh start
