36b7b5fb0624956001d1f0a5ced999cd20c091f9
eagleoflqj <liumeo@pku.edu.cn>
2020-12-09 15:48:52 -0500

    fix dist

--- model.sh
+++ model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-apt install -y python-pip
+sudo apt install -y python3-pip
 python3 -m pip --no-cache-dir install torchvision
 git clone -b final --depth 1 https://github.com/wchen258/cs655-fall2020-network
 cd cs655-fall2020-network
